b63675de39a24c02f07656309a2f8893369e1380
Matej Capkovic <mcapkovic@gmail.com>
2020-08-14 20:32:25 +0200

    Add react app

---
+++ packages/react-app/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import shared from 'app-shared'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      
+        <p>
+        multiply 2 and 3 = {shared.multiply(2, 3)}
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
